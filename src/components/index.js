94908cd82508120d1b07e50c3e4a315f365ddc70
unknown <grisha_kirillov@mail.ru>
2022-10-23 21:01:07 +0300

    Implemented RangePicker component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export {default as EmptyPlaceholder} from './EmptyPlaceholder';
 export {default as SortableTable} from './SortableTable';
 export {default as Notification} from './Notification';
 export {default as SortableList} from './SortableList';
+export {default as DoubleSlider} from './DoubleSlider';
 export {default as RangePicker} from './RangePicker';
 export {default as ColumnChart} from './ColumnChart';
 export {default as Tooltip} from './Tooltip';
--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -1,7 +1,9 @@
-import {SortableTable} from '@/components';
+import {DoubleSlider, SortableTable} from '@/components';
 import {AddProductPage, MainPage} from '@/pages';
-import {API_URL_REST, BACKEND_URL} from '@/utils/settings';
+
 import headerConfig from './headerConfig';
+import {API_URL_REST, BACKEND_URL} from '@/utils/settings';
+import fetchJson from '@/utils/fetch-json';
 
 export default class Page {
   date = new Date();
@@ -13,17 +15,43 @@ export default class Page {
         <h1 class='page-title'>Товары</h1>
         <a href='/products/add' class='button-primary'>Добавить товар</a>
       </div>
+      <div class="content-box content-box_small">
+        <form class="form-inline">
+          <div class="form-group">
+            <label class="form-label">Сортировать по:</label>
+            <input type="text" data-element="filterName" class="form-control" placeholder="Название товара">
+          </div>
+          <div class="form-group" data-element="sliderContainer">
+            <label class="form-label">Цена:</label>
+            </div>
+          <div class="form-group">
+          <label class="form-label">Статус:</label>
+            <select class="form-control" data-element="filterStatus">
+              <option value="" selected="">Любой</option>
+              <option value="1">Активный</option>
+              <option value="0">Неактивный</option>
+            </select>
+          </div>
+        </form>
+      </div>
       <div data-element='productsContainer' class='products-list__container'>
     </div>
     `;
   }
 
+  appendSlider() {
+    const slider = new DoubleSlider({min: 0, max: 4000});
+    
+    this.subElements.sliderContainer.append(slider.element);
+  }
+
   setUrlParams(url) {
     url.searchParams.set('_embed', 'subcategory.category');
     url.searchParams.set('_sort', 'title');
-    url.searchParams.set('_order', 'desc');
     url.searchParams.set('_start', 0);
     url.searchParams.set('_end', 30);
+
+    Object.entries(this.filteringParams).forEach(([param, value]) => url.searchParams.set(param, value));
   }
 
   appendSortableTable() {
@@ -36,17 +64,20 @@ export default class Page {
       isSortLocally: false
     });
     
-    const container = this.element.querySelector('[data-element="productsContainer"]');
-    container.append(this.components.sortableTable.element);
+    this.subElements.productsContainer.append(this.components.sortableTable.element);
   }
 
   render() {
     const element = document.createElement('div');
     element.innerHTML = this.template();
     this.element = element.firstElementChild;
-    this.components = {};
 
+    this.components = {};
+    this.filteringParams = {};
+
+    this.subElements = this.getSubElements();
     this.appendSortableTable();
+    this.appendSlider();
     this.setEventListeners();
   
     return this.element;
@@ -55,6 +86,48 @@ export default class Page {
   setEventListeners() {
     const button = this.element.querySelector('.button-primary');
     button?.addEventListener('click', this.handleAddProduct);
+    this.element.addEventListener('range-select', this.handleRangeSelect);
+    this.subElements.filterName.addEventListener('input', this.handleChangeName);
+    this.subElements.filterStatus.addEventListener('change', this.handleChangeStatus);
+  }
+
+  handleChangeName = async(event) => {
+    const {value} = event.target;
+    const url = new URL(`${API_URL_REST}/products`, BACKEND_URL);
+
+    this.filteringParams = {...this.filteringParams, 'title_like': value};
+    this.setUrlParams(url);
+
+    const data = await fetchJson(url);
+
+    this.components.sortableTable.setData(data);
+    this.components.sortableTable.updateData();
+  }
+
+  handleChangeStatus = async(event) => {
+    const {value} = event.target;
+    const url = new URL(`${API_URL_REST}/products`, BACKEND_URL);
+
+    this.filteringParams = {...this.filteringParams, 'status': value};
+    this.setUrlParams(url);
+
+    const data = await fetchJson(url);
+
+    this.components.sortableTable.setData(data);
+    this.components.sortableTable.updateData();
+  }
+
+  handleRangeSelect = async(event) => {
+    const {from, to} = event.detail;
+    const url = new URL(`${API_URL_REST}/products`, BACKEND_URL);
+
+    this.filteringParams = {...this.filteringParams, 'price_gte': from, 'price_lte': to};
+    this.setUrlParams(url);
+
+    const data = await fetchJson(url);
+
+    this.components.sortableTable.setData(data);
+    this.components.sortableTable.updateData();
   }
 
   async handleAddProduct(event) {
@@ -71,6 +144,16 @@ export default class Page {
     content.innerHTML = '';
     content.append(renderedPage);
     window.history.pushState(null, null, '/products/add');
+  }
+
+  getSubElements() {
+    const res = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      res[subElement.dataset.element] = subElement;
+    }
+    return res;
   }
 
   removeComponents() {
